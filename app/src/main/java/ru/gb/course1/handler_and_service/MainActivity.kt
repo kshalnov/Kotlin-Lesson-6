8feb8e50881178e349542ba2e628b6463cda27a2
Kirill Shalnov <borhammere@gmail.com>
2021-11-14 21:03:26 +0300

    handler thread sample

--- app/src/main/java/ru/gb/course1/handler_and_service/MainActivity.kt
+++ app/src/main/java/ru/gb/course1/handler_and_service/MainActivity.kt
@@ -1,14 +1,45 @@
 package ru.gb.course1.handler_and_service
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import androidx.appcompat.app.AppCompatActivity
 import ru.gb.course1.handler_and_service.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val uiHandler: Handler by lazy { Handler(mainLooper) }
+    private val workerHandler: Handler by lazy { Handler(handlerThread.looper) }
+
+    private val handlerThread: HandlerThread = HandlerThread("math_worker").apply {
+        start()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.goButton.setOnClickListener {
+            val input = binding.inputEditText.text.toString().toInt()
+            binding.inputEditText.text.clear()
+
+            workerHandler.post {
+                val res = input * input
+                val sleepSec = Random.nextInt() % 2 + 1
+                Thread.sleep(sleepSec * 1_000L)
+                uiHandler.post {
+                    binding.resultTextView.text = "${binding.resultTextView.text}\n$res"
+                }
+            }
+        }
     }
+
+    override fun onDestroy() {
+        handlerThread.quit()
+        super.onDestroy()
+    }
+
 }
